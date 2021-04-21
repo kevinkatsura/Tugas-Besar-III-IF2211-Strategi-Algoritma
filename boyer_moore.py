da60f75ea38389c82f3ee1c4194c1dbeb5af24df
Ardovigus <68460784+Ardovigus@users.noreply.github.com>
2021-04-22 00:29:10 +0700

    Update boyer_moore.py

--- boyer_moore.py
+++ boyer_moore.py
@@ -1,4 +1,4 @@
-def algo_boyer_moore(text, pattern):
+def boyer_moore_search(text, pattern):  # --> Fungsi untuk mengembalikan True jika ditemukan dan False jika tidak
     # Inspirasi dari: https://www.geeksforgeeks.org/boyer-moore-algorithm-for-pattern-searching/
     
     # Membuat Dictionary dengan:
@@ -18,6 +18,8 @@ def algo_boyer_moore(text, pattern):
         if (char_index.__contains__(char) == False):
             char_index[char] = -1
 
+    pattern_ditemukan = False
+
     # Prose Pencarian
     # Inisialisasi
     shift = 0
@@ -32,8 +34,9 @@ def algo_boyer_moore(text, pattern):
 
         # Jika semua karakter yang dibandingkan sama
         if (compare_pos < 0):
-            print("Ditemukan pada shift {}".format(shift)) # --> Edit ini untuk output
-
+            # Alternatif I:
+            #print("Ditemukan pada shift {}".format(shift))
+            
             # Geser sejauh panjang pattern atau disesuaikan agar tepat membandingkan karakter terakhir pada text
             if (shift == len(text) - len(pattern)):
                 shift += 1
@@ -42,6 +45,10 @@ def algo_boyer_moore(text, pattern):
                     shift += (len(text) - len(pattern) - shift)
                 else:
                     shift += len(pattern)
+
+            # Alternatif II:
+            pattern_ditemukan = True
+            break
         # Jika ada karakter yang berbeda
         else:
             if (char_index[text[shift + compare_pos]] != -1):
@@ -54,6 +61,57 @@ def algo_boyer_moore(text, pattern):
             # Case 3 (index karakter yang dibandingkan = -1 / tidak ada)
             else:
                 # Geser sejauh panjang pattern atau disesuaikan agar tepat membandingkan karakter terakhir pada text
+                if (shift == len(text) - len(pattern)):
+                    shift += 1
+                else:
+                    if (shift + len(pattern) > len(text) - len(pattern)):
+                        shift += (len(text) - len(pattern) - shift)
+                    else:
+                        shift += len(pattern)
+
+    return pattern_ditemukan
+
+def boyer_moore_print_all(text, pattern): # --> Prosedur untuk menunjukkan seluruh posisi ditemukan
+    char_index = {}
+
+    for i in range(len(pattern)):
+        char_index[pattern[i]] = i
+
+    text_char_set = set()
+
+    for char in text:
+        text_char_set.update(char)
+
+    for char in text_char_set:
+        if (char_index.__contains__(char) == False):
+            char_index[char] = -1
+
+    shift = 0
+
+    while (shift <= len(text) - len(pattern)):
+        compare_pos = len(pattern) - 1
+
+        while (pattern[compare_pos] == text[shift + compare_pos] and compare_pos >= 0):
+            compare_pos -= 1
+
+        if (compare_pos < 0):
+            print("Ditemukan pada shift {}".format(shift))
+            
+            if (shift == len(text) - len(pattern)):
+                shift += 1
+            else:
+                if (shift + len(pattern) > len(text) - len(pattern)):
+                    shift += (len(text) - len(pattern) - shift)
+                else:
+                    shift += len(pattern)
+
+        else:
+            if (char_index[text[shift + compare_pos]] != -1):
+                if (compare_pos >= char_index[text[shift + compare_pos]]):
+                    shift += (compare_pos - char_index[text[shift + compare_pos]])
+                else:
+                    shift += 1
+            else:
                 if (shift == len(text) - len(pattern)):
                     shift += 1
                 else:
