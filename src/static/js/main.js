3c7b0e39e0f489f84209d0a258949c3f82d74d27
kevinkdps <13519216@std.stei.itb.ac.id>
2021-04-28 18:57:19 +0700

    local

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("form").on('submit',function(event) {
+    $("form").on('submit',function(event)  {
         $.ajax({
             data: {
                 message: $('#masukan').val()
@@ -7,14 +7,14 @@ $(document).ready(function() {
             type : 'POST',
             url : '/process'
         })
-        .done(function(data){
+        .done(function(data)  {
             // Menampilkan pesan yang baru saja dikirim oleh user
             var new_entry = document.createElement("div");
             new_entry.id = "pesan";
             new_entry.innerHTML = data.message;
             document.getElementById("middle").appendChild(new_entry);
             document.getElementById("masukan").value = "";
-        }).done(function(data){
+        }).done(function(data)  {
             // Pesan dari BOT
             var new_bot_message = document.createElement("div");
             new_bot_message.id = "BOT";
